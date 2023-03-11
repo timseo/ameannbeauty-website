fb514de82e72fe178603c0d753a6640844ea6cec
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2023-03-11 19:53:50 +0600

    process update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 (function($) {
-    $('svg .path a').mouseenter(function () { 
-        $('.mobile-test .text'+$(this).data('text')).css({display:'block'});
+    $('svg .path a').on('click', function (e) { 
+        e.preventDefault();
+        $('.mobile-test .text'+$(this).data('text')).css({display:'block'}).siblings().attr('style','');
+        $('.process-step-section.modal').show();
+        $('.process-step-section.modal .content').eq($('.mobile-test .text'+$(this).data('text')).index()).css({display:'block'}).siblings().attr('style','');
     });
-    $('svg .path a').mouseleave(function () { 
-        $('.mobile-test .text'+$(this).data('text')).attr('style','');
+    $('.close').on('click', function (e) { 
+        e.preventDefault();
+        $('.process-step-section.modal').hide();
     });
-
+    $('.mobile-test span').on('click', function (e) { 
+        e.preventDefault();
+        $('.process-step-section.modal').show();
+        $('.process-step-section.modal .content').eq($(this).index()).css({display:'block'}).siblings().attr('style','');
+    });
 })(window.jQuery);
 
