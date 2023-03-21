55605a6d8f0cd52e61a32b81dde70e16d267b1ba
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2023-03-22 00:48:03 +0600

    fade

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,16 +18,16 @@
     $('svg .path a').on('click', function (e) {
         e.preventDefault();
         $('.mobile-test .text'+$(this).data('text')).css({display:'block'}).siblings().attr('style','');
-        $('.process-step-section.modal').show();
+        $('.process-step-section.modal').fadeIn(300);
         $('.process-step-section.modal .content').eq($('.mobile-test .text'+$(this).data('text')).index()).css({display:'block'}).siblings().attr('style','');
     });
     $('.close').on('click', function (e) { 
         e.preventDefault();
-        $('.process-step-section.modal').hide();
+        $('.process-step-section.modal').fadeOut(300);
     });
     $('.mobile-test span').on('click', function (e) { 
         e.preventDefault();
-        $('.process-step-section.modal').show();
+        $('.process-step-section.modal').fadeIn(300);
         $('.process-step-section.modal .content').eq($(this).index()).css({display:'block'}).siblings().attr('style','');
     });
 })(window.jQuery);
